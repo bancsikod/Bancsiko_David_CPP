d1a177b3430ae0cd537a31104e036c1d8537dda2
bancsikod <bancsikod@yahoo.com>
2021-12-02 16:31:29 +0200

    HOL A MAIN?

--- lab10/lab10_2/main.cpp
+++ lab10/lab10_2/main.cpp
@@ -3,15 +3,23 @@
 
 int main() {
 
-    Student S(1,"Molnar", "Orsi");
+    Student S(1,"Marko", "Polo");
     S.addGrade("Magyar", 10);
     S.addGrade("Roman", 4.25);
     S.addGrade("Matek", 9.75);
     S.addGrade("Info", 7.6);
     S.computeAverage();
 
-    cout << S;
+    cout << S << endl;
+    cout << S.getGrade("Roman") << endl;
+    map<string, double> a = S.getGrades();
 
+    for (auto i = a.begin(); i != a.end(); ++i) {
+        cout << i->first << " " << i->second << endl;
+    }
+
+    cout << S.getAverage() << endl;
+    cout << S.getId() << " " << S.getFirstName() << " " << S.getLastName() << endl;
 
     return 0;
 }
